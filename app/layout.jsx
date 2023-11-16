beec680814b06d6062cd6a1547d97f902ad7fb23
jorlar <jqern.larsen@gmail.com>
2023-11-16 21:42:17 +0100

    :construction: Add basic setup to project

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import React, { children } from "react";
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promtopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang='eng'>
+      <body>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
